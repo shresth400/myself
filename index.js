96e1bdeead7c610e7eff9ce59485bf993151496b
Pujan Shrestha <shresthapujans400@gmail.com>
2026-01-07 12:19:41 +0545

    Changes made

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function disclaimer() {
-    const agree = confirm("Agree to the Creative Commons License?")
-    if (agree){
-        return
-    }
-    else{
-        window.location.href = "https://creativecommons.org/licenses/"
-    }
+  const photo = document.querySelector(".myself");
+  const agree = confirm("Agree to the Creative Commons License?");
+  if (agree) {
+    photo.style.display = "block";
+  } else {
+    window.location.href = "https://creativecommons.org/licenses/";
+  }
 }
 
 window.onload = disclaimer;
